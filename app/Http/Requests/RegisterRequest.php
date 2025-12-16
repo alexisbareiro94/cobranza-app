2600437dbb2381d3d4afa32dd7afc7adbdd7d8a0
Alexis Bareiro <alexisblugo@gmail.com>
2025-12-16 15:31:37 -0300

    feat: increase password complexity requirements and update validation messages.

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -26,20 +26,26 @@ class RegisterRequest extends FormRequest
         return [
             'name' => 'required',
             'email' => 'required|email|unique:users,email',
-            'password' => 'required|min:6'
+            'password' => [
+                'required',
+                'min:8',
+                'regex:/[A-Z]/',      // Al menos una letra mayúscula
+                'regex:/[0-9]/',      // Al menos un número
+                'regex:/[@$!%*?&#]/', // Al menos un carácter especial
+            ]
         ];
     }
 
     public function messages(): array
     {
         return [
-            'name.required' => 'El nombre el requerido',
+            'name.required' => 'El nombre es requerido',
             'email.required' => 'El email es requerido',
-            'email.email' => 'Ingrese un email valido',
-            'email.unique' => 'El email: ' . $this->email . ' Ya esta registrado',
+            'email.email' => 'Ingrese un email válido',
+            'email.unique' => 'El email: ' . $this->email . ' ya está registrado',
             'password.required' => 'La contraseña es requerida',
-            'password.min' => 'La contraseña debe tener al menos 6 caracteres',
+            'password.min' => 'La contraseña debe tener al menos 8 caracteres',
+            'password.regex' => 'La contraseña debe contener al menos una letra mayúscula, un número y un carácter especial (@$!%*?&#)',
         ];
     }
-
 }
